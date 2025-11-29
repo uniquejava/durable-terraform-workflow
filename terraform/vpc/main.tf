ee471d9225eb7774367a25db5e0c70c01a9f8911
robertdippolito <robert.e.dippolito@gmail.com>
2025-11-28 20:30:55 -0500

    updated vpc module

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -13,6 +13,45 @@ resource "aws_vpc" "vpc" {
   enable_dns_hostnames = true
 
   tags = {
-    Name = "dev-vpc-temporal"
+    Name = "${var.environment}-vpc"
   }
+}
+
+resource "aws_subnet" "vpc-public" {
+  count             = length(var.azs)
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index)
+  availability_zone = var.azs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.environment}-public-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_internet_gateway" "k8s-api-igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.environment}-igw"
+  }
+}
+
+resource "aws_route_table" "vpc-public-rt" {
+    vpc_id = aws_vpc.vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.k8s-api-igw.id
+    }
+
+    tags = {
+        Name = "${var.environment}-public-rt"
+    }
+}
+
+resource "aws_route_table_association" "vpc-public-rt-a" {
+  count          = length(aws_subnet.vpc-public)
+  subnet_id      = aws_subnet.vpc-public[count.index].id
+  route_table_id = aws_route_table.vpc-public-rt.id
 }
--- terraform/vpc/variables.tf
+++ terraform/vpc/variables.tf
@@ -2,3 +2,14 @@ variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   default     = "10.1.0.0/16"
 }
+
+variable "environment" {
+  description = "environment"
+  default = "temporal-dev"
+}
+
+variable "azs" {
+  description = "List of availability zones in the region"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
